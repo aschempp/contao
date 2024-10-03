4b76f8e17fa55e51d42885fba870579ce6832fc3
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-10-03 17:53:06 +0200

    Restore touch handling in deeplink controller

--- core-bundle/assets/controllers/deeplink-controller.js
+++ core-bundle/assets/controllers/deeplink-controller.js
@@ -37,29 +37,55 @@ export default class extends Controller {
     }
 
     initialize () {
-        this.visitPrimary = this.visitPrimary.bind(this);
-        this.visitSecondary = this.visitSecondary.bind(this);
+        this.handle = this.handle.bind(this);
     }
 
     connect () {
-        this.element.addEventListener('click', this.visitPrimary);
-        this.element.addEventListener('click', this.visitSecondary);
+        this.element.addEventListener('click', this.handle);
     }
 
     disconnect () {
-        this.element.removeEventListener('click', this.visitPrimary);
-        this.element.removeEventListener('click', this.visitSecondary);
+        this.element.removeEventListener('click', this.handle);
+    }
+
+    handle (event) {
+        // Ignore clicks on anchor elements
+        if (!this.isValid(event.target)) {
+            return;
+        }
+
+        const primaryKey = window.navigator.platform?.startsWith('Mac') ? 'metaKey' : 'ctrlKey';
+
+        if (event[primaryKey] && this.hasPrimaryTarget && this.primaryTarget.href) {
+            Turbo.visit(this.primaryTarget.href);
+            return;
+        }
+
+        if (event.shiftKey && this.hasSecondaryTarget && this.secondaryTarget.href) {
+            Turbo.visit(this.secondaryTarget.href);
+            return;
+        }
+
+        if (event.pointerType === 'mouse' || !this.hasPrimaryTarget || !this.primaryTarget.href) {
+            return;
+        }
+
+        if (!this.element.getAttribute('data-visited')) {
+            this.element.setAttribute('data-visited', '1');
+        } else {
+            this.element.removeAttribute('data-visited');
+            Turbo.visit(this.primaryTarget.href);
+        }
     }
 
     visitPrimary (event) {
-        const key = window.navigator.platform?.startsWith('Mac') ? 'metaKey' : 'ctrlKey';
-        if (this.hasPrimaryTarget && this.primaryTarget.href && event[key] && this.isValid(event.target)) {
+        if (this.hasPrimaryTarget && this.primaryTarget.href && this.isValid(event.target)) {
             Turbo.visit(this.primaryTarget.href);
         }
     }
 
     visitSecondary (event) {
-        if (this.hasSecondaryTarget && this.secondaryTarget.href && event.shiftKey && this.isValid(event.target)) {
+        if (this.hasSecondaryTarget && this.secondaryTarget.href && this.isValid(event.target)) {
             Turbo.visit(this.secondaryTarget.href);
         }
     }
